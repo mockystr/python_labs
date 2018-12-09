fafec6055564b2b7aac900d4b1255c0f9d1fce2e
Emir <navruzov.e@mail.ru>
2018-12-09 13:08:03 +0000

    deploy1

--- homework/homework/settings.py
+++ homework/homework/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '4y^typulskls%li*vz%a-rues-+ndmz=bn2!doh%yunv4131jj'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['104.248.244.37',]
 
 # Application definition
 
